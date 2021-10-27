2c3a86d6238205f44afb57c648bdbeef0e10e082
ADMIN <wlsdn2749@naver.com>
2021-10-28 01:25:56 +0900

    3-10 complete!

--- pybo/models.py
+++ pybo/models.py
@@ -12,10 +12,17 @@ class Question(models.Model):
     def __str__(self):
         return self.subject
 
-
 class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
     content = models.TextField()
     modify_date = models.DateTimeField(null=True, blank=True)
     create_date = models.DateTimeField()
